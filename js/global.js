f4b27600ea38d76f7d19ace24e2e26e2eee19b58
Kenneth Frantzen <5168530+seetee@users.noreply.github.com>
2024-02-22 11:16:09 +0100

    Initial commit

---
+++ js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.shr2024 = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(Drupal);
